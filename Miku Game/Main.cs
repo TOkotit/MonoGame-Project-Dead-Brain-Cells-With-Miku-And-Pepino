30a8250bf46cf0eb50ccb6ff1e613cb76284c1d5
TOkotit <tokotit32@gmail.com>
2025-03-30 21:31:21 +0500

    Началработу. Monogae это буквально Pygame, тщ на шарпе

---
+++ Miku Game/Main.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Linq;
+
+namespace Miku_Game;
+
+public class Main : Game
+{
+
+    public static float Gravity;
+    public Player Miku;
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+
+
+    public Main()
+    {
+        Miku = new();
+        Gravity = 200f;
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = (int)(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width * 0.95);
+        _graphics.PreferredBackBufferHeight = (int)(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height * 0.9);
+        _graphics.HardwareModeSwitch = false;
+        _graphics.IsFullScreen = false;
+        _graphics.ApplyChanges();
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        Miku.Texture = Content.Load<Texture2D>("Player textures/miku_test_texture");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                             Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        var kstate = Keyboard.GetState();
+
+
+
+        if (kstate.IsKeyDown(Keys.Up))
+            Miku.Velocity.Y -= Gravity * 0.9f;
+
+        if (kstate.IsKeyDown(Keys.Left))
+            Miku.Velocity.X -= Miku.StandartSpeed;
+
+        if (kstate.IsKeyDown(Keys.Right))
+            Miku.Velocity.X += Miku.StandartSpeed;
+
+
+
+
+
+        if (Miku.Position.X > _graphics.PreferredBackBufferWidth - Miku.Texture.Width / 2)
+            Miku.Position.X = _graphics.PreferredBackBufferWidth - Miku.Texture.Width / 2;
+
+        else if (Miku.Position.X < Miku.Texture.Width / 2)
+            Miku.Position.X = Miku.Texture.Width / 2;
+
+        if (Miku.Position.Y > _graphics.PreferredBackBufferHeight - Miku.Texture.Height / 2)
+            Miku.Position.Y = _graphics.PreferredBackBufferHeight - Miku.Texture.Height / 2;
+
+        else if (Miku.Position.Y < Miku.Texture.Height / 2)
+            Miku.Position.Y = Miku.Texture.Height / 2;
+
+        Miku.Update(gameTime);
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.BlueViolet);
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(
+            Miku.Texture,
+            Miku.Position,
+            null,
+            Color.White,
+            0f,
+            new Vector2(Miku.Texture.Width / 2, Miku.Texture.Height / 2),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+        );
+        _spriteBatch.End();
+
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Miku Game/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Miku_Game 
+{   
+
+
+    public class Player : GameObject 
+    {
+        public override void Update(GameTime gameTime)
+        {
+            if (Velocity.X != 0)
+                Velocity.X -= 10f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (Math.Abs(Velocity.X) < 1)
+                Velocity.X = 0;
+            base.Update(gameTime);
+        }
+    }
+}
